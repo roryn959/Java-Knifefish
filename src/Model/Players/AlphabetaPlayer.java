8d451c9dcdcbfd3db78657fa047af5e44c5d37f3
roryn959 <zcabrbn@ucl.ac.uk>
2022-05-29 18:29:30 +0100

    Made advanced eval. Appears to work

--- src/Model/Players/AlphabetaPlayer.java
+++ src/Model/Players/AlphabetaPlayer.java
@@ -11,7 +11,7 @@ public class AlphabetaPlayer implements PlayerInterface {
     private final int DEPTH = 6;
     public AlphabetaPlayer(Board board){
         this.board = board;
-        this.evaluator = new SimpleEvaluation(board);
+        this.evaluator = new AdvancedEvaluation(board);
     }
 
     public Move findMove(){
